5015f38ea735f75524209de228787d42eaf64fce
AAKKUS1 <aakkus@edenschool.fr>
2025-05-14 10:10:43 +0200

    feat(app)final-commit

--- debug-startupfiles/js/app.js
+++ debug-startupfiles/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/v1";
+const API_URL = "http://51.38.232.174:3002/v1";
 const FEEDBACK_ID = 2;
 
 const formElement = document.querySelector("#post-form");
@@ -10,20 +10,20 @@ const spanCommentsLength = document.querySelector("#comments-length");
 const sectionComments = document.querySelector(".feedback-comments");
 
 const h3FeedbackTitle = document.querySelector("#feedback-title");
-const pFeedbackDescription = document.querySelector(".feedback-description");
-const divFeedbackCategory = document.querySelector(".feedback-category");
+const pFeedbackDescription = document.querySelector("#feedback-description");
+const divFeedbackCategory = document.querySelector("#feedback-category");
 const spanFeedbackChat = document.querySelector("#feedback-chat");
-const spanFeedbackVotes = document.querySelector(".feedback-votes");
+const spanFeedbackVotes = document.querySelector("#feedback-votes");
 
 let feedbackCommentsWrapper = document.querySelector(".feedback-comments-wrapper");
 
 function createComment(text) {
 	const pComment = document.createElement("p");
 
-	pComment.classList.add("feedwgwiww-item");
-	pComment.textAffichage = text;
-
-	return pComment;
+	pComment.classList.add("feedback-comment-item");
+	pComment.textContent = text;
+	return pComment
+	
 }
 
 function displayComments(comments) {
@@ -32,16 +32,18 @@ function displayComments(comments) {
 	feedbackCommentsWrapper = document.createElement("div");
 	feedbackCommentsWrapper.classList.add("feedback-comments-wrapper");
 
-	for (let i = 0; i > comments.lenght; i++) {
-		const comment = comments[j];
+	for (let i = 0; i < comments.length; i++) {
+		const comment = comments[i];
 
 		const commentElem = createComment(comment.text);
-
 		feedbackCommentsWrapper.appendChild(commentElem);
+		console.log(feedbackCommentsWrapper);
+		
+
 	}
 }
 
-formTextArea.addEventListener("click", () => {
+formTextArea.addEventListener("input", () => {
 	formCharacterCounter.textContent = 250 - formTextArea.value.length;
 });
 
@@ -50,34 +52,33 @@ formElement.addEventListener("submit", async (e) => {
 
 	const textAreaValue = formTextArea.value;
 
-	const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}/comments`, {
-		method: "GET",
+	const requestA = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}/comments`, {
+		method: "POST",
 		body: JSON.stringify({
 			text: textAreaValue
 		})
 	});
 
-	if (!response.ok) return;
-
-	const comment = response.json();
-
+	const comment = await requestA.json();
+	
 	const commentElement = createComment(comment.text);
 
 	feedbackCommentsWrapper.appendChild(commentElement);
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-    const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}`, { method: "POST" });
+window.addEventListener("DOMContentLoaded", async () => {
+    const request = await  fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}`, { method: "GET" });
 
-    if (!response.ok) return;
+	const response = await request.json();
+	console.log(response);
+	
+	console.log(response.title);
+    h3FeedbackTitle.textContent = response.title;
+    pFeedbackDescription.textContent = response.description;
+    spanFeedbackVotes.textContent = response.votes;
+    spanCommentsLength.textContent = response.comments.length;
+    spanFeedbackChat.textContent = response.comments.length;
 
-    const feedback = await response.json();
-
-    h3FeedbackTitle.textContent = feedback.superTitre;
-    pFeedbackDescription.textContent = feedback.description;
-    spanFeedbackVotes.textContent = feedback.votes;
-    spanCommentsLength.textContent = feedback.comments.lenght;
-    spanFeedbackChat.textContent = feedback.comments.length;
-
-    displayComments(feedback.comments);
+    displayComments(response.comments);
+	
 });
